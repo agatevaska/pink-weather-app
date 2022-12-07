75b9a854073720cfc63a5c6a9c1d3c7dd4012e62
agatevaska <vaskaagate@gmail.com>
2022-12-07 17:50:19 +0200

    Added current day and time on js, added the api for the search bar, added js for current temperature, humidity and wind speed

---
+++ src/app.js
@@ -0,0 +1,73 @@
+function showDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayNumber = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayNumber];
+  return `${day} ${hours}:${minutes}`;
+}
+let dateElement = document.querySelector("#date-time");
+let currentTime = new Date();
+dateElement.innerHTML = "Last updated " + showDate(currentTime);
+
+function getLocation(position) {
+  let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function currentGeoLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getLocation);
+}
+
+let geoLocationButton = document.querySelector("#weather-refresh");
+geoLocationButton.addEventListener("click", currentGeoLocation);
+
+function showWeather(response) {
+  document.querySelector("#h1").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+
+  document.querySelector("#wind-speed").innerHTML =
+    "Wind speed: " + Math.round(response.data.wind.speed) + " km/h";
+
+  document.querySelector("#humidity").innerHTML =
+    "Humidity: " + Math.round(response.data.main.humidity) + "%";
+}
+
+function citySearch(city) {
+  let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  citySearch(city);
+}
+let searchForm = document.querySelector("#search-bar");
+searchForm.addEventListener("submit", handleSubmit);
